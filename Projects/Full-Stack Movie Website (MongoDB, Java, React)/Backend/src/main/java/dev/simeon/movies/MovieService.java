3054ca8ef88e447097d29d3556ce4d2aa2046015
simrubin <149342852+simrubin@users.noreply.github.com>
2025-01-17 12:35:55 +1100

    changes to movie web app

--- Projects/Full-Stack Movie Website (MongoDB, Java, React)/Backend/src/main/java/dev/simeon/movies/MovieService.java
+++ Projects/Full-Stack Movie Website (MongoDB, Java, React)/Backend/src/main/java/dev/simeon/movies/MovieService.java
@@ -2,7 +2,6 @@ package dev.simeon.movies;
 
 import java.util.List;
 import java.util.Optional;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
