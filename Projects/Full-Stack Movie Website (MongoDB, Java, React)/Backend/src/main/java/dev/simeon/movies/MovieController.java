b14561d8de17e9e29173b8435a44b6d1fcd61d09
simrubin <149342852+simrubin@users.noreply.github.com>
2025-01-17 12:56:13 +1100

    changes

--- Projects/Full-Stack Movie Website (MongoDB, Java, React)/Backend/src/main/java/dev/simeon/movies/MovieController.java
+++ Projects/Full-Stack Movie Website (MongoDB, Java, React)/Backend/src/main/java/dev/simeon/movies/MovieController.java
@@ -2,7 +2,6 @@ package dev.simeon.movies;
 
 import java.util.List;
 import java.util.Optional;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
